476cb9ad242ab8d0d34f4034bb4d48fcc03fa0a2
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-19 23:39:22 -0300

    zod validation messages

---
+++ src/lib/zod/zod-messages.ts
@@ -0,0 +1,14 @@
+import i18next from 'i18next'
+
+export const zodMessages = {
+  required: () => i18next.t('validation.required'),
+  string: {
+    min: (min: number) => i18next.t('validation.minLength', { min }),
+    max: (max: number) => i18next.t('validation.maxLength', { max }),
+    email: () => i18next.t('validation.email'),
+    url: () => i18next.t('validation.url'),
+    onlyLettersAndSpaces: () => i18next.t('validation.onlyLettersAndSpaces'),
+    onlyLettersNumbersUnderscores: () =>
+      i18next.t('validation.onlyLettersNumbersUnderscores'),
+  },
+}
