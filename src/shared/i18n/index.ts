6c15218aad6bee5f865cce83451a6a1fcc2a931f
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-19 23:41:17 -0300

    feat: internationalization

---
+++ src/shared/i18n/index.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import enUS from './locales/en-US.json'
+import ptBR from './locales/pt-BR.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en-US',
+    resources: {
+      'en-US': { translation: enUS },
+      'pt-BR': { translation: ptBR },
+    },
+  })
+
+export default i18n
