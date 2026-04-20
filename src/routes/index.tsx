78d5df00ae5beac9b121effd1fdb88799e3fe563
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-19 23:40:08 -0300

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { RegisterPage } from '../features/auth/pages/register'
+import { EmailConfirmation } from '@/features/auth/pages/email-confirmation'
+import { routes } from './routes-paths'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RegisterPage />} />
+        <Route path={routes.register} element={<RegisterPage />} />
+        <Route
+          path={routes.emailConfirmation}
+          element={<EmailConfirmation />}
+        />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/routes/routes-paths.ts
@@ -0,0 +1,5 @@
+export const routes = {
+  register: '/auth/register',
+  signin: '/auth/signin',
+  emailConfirmation: '/auth/email-confirmation',
+}
