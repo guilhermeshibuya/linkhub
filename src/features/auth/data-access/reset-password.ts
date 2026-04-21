1764fb853cb0e7e071fd07b9914125e8ee044bb2
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-21 20:26:42 -0300

    refactor: update reset password function to use route object

--- src/features/auth/data-access/reset-password.ts
+++ src/features/auth/data-access/reset-password.ts
@@ -1,8 +1,9 @@
+import { routes } from '@/routes/routes-paths'
 import { supabase } from '@/services/supabse'
 
 export async function sendResetPasswordEmail(email: string) {
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${window.location.origin}/auth/change-password`,
+    redirectTo: `${window.location.origin}${routes.changePassword}`,
   })
 
   return { error }
