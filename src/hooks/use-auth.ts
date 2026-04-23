501423046fcfcfbb16b8df10252dce64d4274849
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-23 11:36:10 -0300

    feat: auth provider

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,27 +1,8 @@
-import { supabase } from '@/services/supabse'
-import type { User } from '@supabase/supabase-js'
-import { useEffect, useState } from 'react'
+import { AuthContext } from '@/providers/auth/auth-context'
+import { useContext } from 'react'
 
 export function useAuth() {
-  const [user, setUser] = useState<User | null>(null)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setUser(session?.user ?? null)
-      setLoading(false)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  const signout = () => supabase.auth.signOut()
-
-  return { user, loading, signout }
+  const context = useContext(AuthContext)
+  if (!context) throw new Error('useAuth must be used within an AuthProvider')
+  return context
 }
