f5c06a92a070906e08706a93477ec94a7f575828
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-04-29 20:52:30 -0300

    react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { AppRoutes } from './routes'
 import { ThemeProvider } from './providers/theme/theme-provider'
 import { AuthProvider } from './providers/auth/auth-provider'
 import { Toaster } from './components/ui/sonner'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <AppRoutes />
-        <Toaster />
-      </ThemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <AppRoutes />
+          <Toaster />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
